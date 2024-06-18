01cb220e8fd25ad37bae3223a560b99afd163d01
Corey Hemminger <hemminger@hotmail.com>
2024-06-18 12:26:47 -0500

    test
    Signed-off-by: Corey Hemminger <hemminger@hotmail.com>

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -23,17 +23,9 @@ describe Kitchen::Driver::WORKFLOW_TESTING_VERSION do
     expect(defined?(Kitchen::Driver::WORKFLOW_TESTING_VERSION)).to be_truthy
   end
 
-  it "has a STRING constant" do
-    expect(Kitchen::Driver::WORKFLOW_TESTING_VERSION.const_defined?(:STRING)).to be_truthy
-  end
-
-  it "has a CURRENT constant" do
-    expect(Kitchen::Driver::WORKFLOW_TESTING_VERSION.const_defined?(:CURRENT)).to be_truthy
-  end
-
   it "compares versions correctly" do
-    version_1 = Kitchen::Driver::WORKFLOW_TESTING_VERSION::CURRENT
-    version_2 = Kitchen::Driver::WORKFLOW_TESTING_VERSION::CURRENT
+    version_1 = Kitchen::Driver::WORKFLOW_TESTING_VERSION
+    version_2 = Kitchen::Driver::WORKFLOW_TESTING_VERSION
     expect(version_1).to eq(version_2)
   end
 end
